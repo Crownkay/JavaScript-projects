ae5fcb13ed76afca28a4c602c4dc3427df429866
Crownkay <adekomsk@gmail.com>
2021-10-17 07:28:18 +0100

    question title & answer  collaspe

---
+++ Questions/app.js
@@ -0,0 +1,11 @@
+// using selector inside the Element
+// traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+});
